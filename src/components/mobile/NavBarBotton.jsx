dbf0af44cda0d0fc63d212c8305566b4f0d6a741
samito <martinsamuelpp@gmail.com>
2024-10-21 19:03:41 -0600

    Refactorizar componentes y estilos en NavBarBotton.jsx

--- src/components/mobile/NavBarBotton.jsx
+++ src/components/mobile/NavBarBotton.jsx
@@ -1,19 +1,40 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { GoHomeFill } from "react-icons/go";
+import { FiSearch } from "react-icons/fi";
+import { BiLibrary } from "react-icons/bi";
+import { FaSpotify } from "react-icons/fa";
+
 
 const NavBarBotton = () => {
-    
+
+    const sizeIcons = '1.6rem'
+
   return (
     <main className="fixed bottom-0 w-full">
       <Tabs defaultValue="inicio" className="w-full">
-        <TabsList className="w-full">
-          <TabsTrigger value="inicio">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
+        <TabsList className="w-full h-16 bg-white flex justify-around px-5">
+          <TabsTrigger className="flex flex-col" value="inicio">
+            <GoHomeFill size={sizeIcons} />
+            <p>Inicio</p>
+          </TabsTrigger>
+          <TabsTrigger className="flex flex-col" value="search">
+            <FiSearch size={sizeIcons} />
+            <p>Buscar</p>
+          </TabsTrigger>
+          <TabsTrigger className="flex flex-col" value="library">
+            <BiLibrary size={sizeIcons} />
+            <p>Tu biblioteca</p>
+          </TabsTrigger>
+          <TabsTrigger className="flex flex-col" value="premium">
+            <FaSpotify size={sizeIcons} />
+            <p>Spotify</p>
+          </TabsTrigger>
         </TabsList>
         <div className="fixed top-0">
-          <TabsContent value="inicio">
-            Make changes to your account here.
-          </TabsContent>
-          <TabsContent value="password">Change your password here.</TabsContent>
+          <TabsContent value="inicio">Inicio</TabsContent>
+          <TabsContent value="search">Search</TabsContent>
+          <TabsContent value="library">Library</TabsContent>
+          <TabsContent value="premium">Premium</TabsContent>
         </div>
       </Tabs>
     </main>

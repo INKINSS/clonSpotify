fbb7ca09bcb8186c5dcee2e77deabeb179f46f28
samito <martinsamuelpp@gmail.com>
2024-10-24 16:23:37 -0600

    Refactorizar configuración de Firebase y añadir gestión de estado de pestañas activas

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore"
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,11 +15,13 @@ const firebaseConfig = {
   messagingSenderId: "274270342931",
   appId: "1:274270342931:web:d7fb1d7e538ef428c814e6",
   measurementId: "G-HTYDS58ZC6",
+  databaseURL:"https://clonspotify-87560-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getFirestore(app);
+const database = getDatabase(app);
 const storage = getStorage(app);
+
 
 export { app, database, storage };

3a2a360e5598590204fe5219b7a5f7095152d97b
samito <martinsamuelpp@gmail.com>
2024-10-24 16:24:14 -0600

    componentes traidos de firebase y mostrados en vista para movil

--- src/views/mobile/ContentForUser.jsx
+++ src/views/mobile/ContentForUser.jsx
@@ -1,8 +1,44 @@
+import { ref, onValue } from "firebase/database";
+import { database } from "../../../firebase";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const ContentForUser = () => {
-  return (
-    <div>ContentForUser</div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default ContentForUser
+  useEffect(() => {
+    const data = ref(database, "recommend");
+    onValue(data, (snapshot) => {
+      const data = snapshot.val();
+      setData(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="text-customWhite text-[1.5rem] mb-5 font-bold">
+        Hecho para Ti
+      </h2>
+      <section className="flex space-x-4 w-full px-[1rem] overflow-auto no-scrollbar">
+        {data.map((el) => (
+          <article className="w-[8rem] h-[10rem]" key={el.id}>
+            <figure className="w-[8rem] h-[8rem]">
+              <img
+                className="w-full h-full object-cover"
+                src={el.urlPoster}
+                alt={el.title}
+              />
+            </figure>
+            <div className="mt-2 w-full">
+              <span className=" w-full text-[.5rem] h-[3rem] inline-block overflow-hidden text-grayExtraLight">
+                {el.footerDescription}
+              </span>
+            </div>
+          </article>
+        ))}
+      </section>
+    </>
+  );
+};
+
+export default ContentForUser;

3a774bf84519460d825c0d9c5dc8d51edb4cbe34
samito <martinsamuelpp@gmail.com>
2024-10-24 17:21:37 -0600

    - Refactorizar configuración de WaveDown y ContentForUser - cambiar color y decoracion de texto según base de datos

--- src/common/WaveDown.jsx
+++ src/common/WaveDown.jsx
@@ -1,8 +1,8 @@
 const WaveDown = () => {
   return (
     <svg
-      className="absolute bottom-0 object-cover"
-      height={"6rem"}
+      className="absolute bottom-0 object-cover overflow-hidden"
+      height={"4.4rem"}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1440 320"
     >
--- src/views/mobile/ContentForUser.jsx
+++ src/views/mobile/ContentForUser.jsx
@@ -2,6 +2,7 @@ import { ref, onValue } from "firebase/database";
 import { database } from "../../../firebase";
 import { useEffect } from "react";
 import { useState } from "react";
+import WaveDown from "../../common/WaveDown";
 
 const ContentForUser = () => {
   const [data, setData] = useState([]);
@@ -21,16 +22,27 @@ const ContentForUser = () => {
       </h2>
       <section className="flex space-x-4 w-full px-[1rem] overflow-auto no-scrollbar">
         {data.map((el) => (
-          <article className="w-[8rem] h-[10rem]" key={el.id}>
-            <figure className="w-[8rem] h-[8rem]">
+          <article
+            className="w-[8rem] overflow-visible relative"
+            key={el.id}
+          >
+            <figure className="w-[8rem] h-[8rem] relative overflow-hidden">
               <img
                 className="w-full h-full object-cover"
                 src={el.urlPoster}
                 alt={el.title}
               />
+              {el.isDecoration ? <WaveDown /> : ""}
+              <h3
+                className={`absolute bottom-[.5rem] leading-[.8rem] left-[.4rem] font-bold w-full text-[.7rem] inline-block overflow-hidden ${
+                  el.isTextBlack ? "text-black" : "text-white"
+                }`}
+              >
+                {el.titlePoster}
+              </h3>
             </figure>
             <div className="mt-2 w-full">
-              <span className=" w-full text-[.5rem] h-[3rem] inline-block overflow-hidden text-grayExtraLight">
+              <span className="text-grayExtraLight w-full text-[.5rem] leading-[.8rem] inline-block">
                 {el.footerDescription}
               </span>
             </div>
